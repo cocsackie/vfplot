75023da8b487e91c7ef0c62c3490c2c1a862a385
J.J. Green,,, <j.j.green@gmx.co.uk>
2007-06-03 20:30:21 +0000

    *** empty log message ***

--- src/libvfplot/adaptive.c
+++ src/libvfplot/adaptive.c
@@ -2,7 +2,7 @@
   adaptive.c
   vfplot adaptive plot 
   J.J.Green 2007
-  $Id: adaptive.c,v 1.6 2007/05/31 23:28:50 jjg Exp jjg $
+  $Id: adaptive.c,v 1.7 2007/06/01 22:52:36 jjg Exp jjg $
 */
 
 #include <math.h>
@@ -12,6 +12,14 @@
 #include <vfplot/evaluate.h>
 #include <vfplot/matrix.h>
 #include <vfplot/limits.h>
+
+/* number of iterations in placement */
+
+#define DIM0_POS_ITER  4
+
+/* sine of smallest angle for acute placement (10 degrees) */
+
+#define DIM0_ACUTE_MIN 0.173648
 
 /* 
    add-hoc structure to carry our state through the 
@@ -27,9 +35,11 @@ typedef struct
   int N;
   int *K;
   arrow_t* A;
+  ellipse_t e;
 } dim0_opt_t;
 
 static int dim0(domain_t*,dim0_opt_t*,int);
+static int mean_ellipse(domain_t*,vfun_t,cfun_t,void*,ellipse_t*);
 
 extern int vfplot_adaptive(domain_t* dom,
 			   vfun_t fv,
@@ -39,26 +49,106 @@ extern int vfplot_adaptive(domain_t* dom,
 			   int N,
                            int *K,arrow_t* A)
 {
-  dim0_opt_t dim0_opt = {fv,fc,field,opt,N,K,A};
-
   if (opt.verbose)  printf("adaptive placement\n");
 
-  *K = 0;
+  int err;
+
+  /* mean ellipse */
+
+  ellipse_t me = {0};
+
+  if ((err = mean_ellipse(dom,fv,fc,field,&me)) != ERROR_OK)
+    return err;
+
+  if (opt.verbose) printf("mean ellipse: major %.3g minor %.3g\n",me.major,me.minor);
 
   /* 
      dimension zero, here we place a glyph at the interior
      of each corner in the domain.
   */
 
-  int err = domain_iterate(dom,(difun_t)dim0,&dim0_opt);
+  if (opt.verbose) printf("dimension zero\n");
 
-  if (err)
+  dim0_opt_t dim0_opt = {fv,fc,field,opt,N,K,A,me};
+
+  *K = 0;
+
+  if ((err = domain_iterate(dom,(difun_t)dim0,&dim0_opt)) != ERROR_OK)
     {
       fprintf(stderr,"failed generation at dimension zero\n");
       return err;
     }
 
-  if (opt.verbose) printf("dim 0 : %i\n",*K);
+  if (opt.verbose) printf("initial %i\n",*K);
+
+  return ERROR_OK;
+}
+
+/* 
+   this samples the objective an an NxN grid and
+   finds the mean ellipse size - useful for starting
+   values of placement iterations
+*/
+
+static int mean_ellipse(domain_t *dom,
+			vfun_t fv,
+			cfun_t fc,
+			void* field,
+			ellipse_t* pe)
+{
+  double N = 10;
+  bbox_t bb = domain_bbox(dom);
+  double smaj = 0.0, smin = 0.0,
+    w  = bb.x.max - bb.x.min,
+    h  = bb.y.max - bb.y.min,
+    x0 = bb.x.min,
+    y0 = bb.y.min;
+
+  int i,k=0;
+  double dx = w/N, dy = h/N;
+
+  for (i=0 ; i<N ; i++)
+    {
+      double x = x0 + (i + 0.5)*dx;
+      int j;
+      
+      for (j=0 ; j<N ; j++)
+        {
+          double y = y0 + (j + 0.5)*dy;
+          vector_t v = {x,y};
+
+          if (! domain_inside(v,dom)) continue;
+
+          arrow_t A;
+
+          A.centre = v;
+
+          int err = evaluate(&A,fv,fc,field);
+
+          switch (err)
+            {
+            case ERROR_OK : k++ ; break;
+            case ERROR_NODATA: break;
+            default: return err;
+            }
+
+	  ellipse_t e;
+
+	  if (arrow_ellipse(&A,&e) != 0) return ERROR_BUG;
+
+	  smaj += e.major;
+	  smin += e.minor;
+        }
+    }
+
+  if (!k)
+    {
+      fprintf(stderr,"failed to find mean ellipse size (strange domain?)\n");
+      return ERROR_BUG;
+    }
+
+  pe->major = smaj/k;
+  pe->minor = smin/k;
 
   return ERROR_OK;
 }
@@ -97,8 +187,6 @@ static int dim0(domain_t* dom,dim0_opt_t* opt,int L)
 
 static vector_t intersect(vector_t,vector_t,double,double);
 
-#define DIM0_POS_ITER 4
-
 static int dim0_corner(vector_t a,vector_t b,vector_t c,dim0_opt_t* opt)
 {
   vector_t u = vsub(b,a), v = vsub(c,b);
@@ -114,9 +202,11 @@ static int dim0_corner(vector_t a,vector_t b,vector_t c,dim0_opt_t* opt)
     
   A->centre = b;
 
+  double em = (opt->e.minor + opt->e.major)/2.0;
+
   int num = DIM0_POS_ITER;
 
-  if (sin(t2-t1) > 0.1)
+  if (sin(t2-t1) > DIM0_ACUTE_MIN)
     {
       /* 
 	 acute (snook) 
@@ -129,16 +219,21 @@ static int dim0_corner(vector_t a,vector_t b,vector_t c,dim0_opt_t* opt)
       vector_t u1 = vunit(u), v1 = vunit(v);
       m2_t N = {-v1.y, v1.x, -u1.y, u1.x};
 
+      /* 
+	 starting point is b + c, where c = (em,em)
+	 in u-v coordinates
+      */
+
+      vector_t w = {em,em};
+
+      A->centre = vadd(b,m2vmul(N,w));
+
       do 
 	{
-	  int i,err = evaluate(A,opt->fv,opt->fc,opt->field);
-	  
-	  switch (err)
-	    {
-	    case ERROR_OK: case ERROR_NODATA: break;
-	    default: 
-	      return err;
-	    }
+	  int i,err;
+
+	  if ((err = evaluate(A,opt->fv,opt->fc,opt->field)) != ERROR_OK)
+	    return err;
 	  
 	  ellipse_t e;
 	  
@@ -174,18 +269,23 @@ static int dim0_corner(vector_t a,vector_t b,vector_t c,dim0_opt_t* opt)
 	 tangent at this angle 
       */
 
-      m2_t N = {ct3,st3,-st3,ct3};
+      m2_t N = {-ct3,-st3,-st3,ct3};
+
+      /* 
+	 starting point is b + c, where c = (em,0)
+	 in median coordinates
+      */
+
+      vector_t w = {0,em};
+
+      A->centre = vadd(b,m2vmul(N,w));
 
       do 
 	{
-	  int i,err = evaluate(A,opt->fv,opt->fc,opt->field);
-	  
-	  switch (err)
-	    {
-	    case ERROR_OK: case ERROR_NODATA: break;
-	    default: 
-	      return err;
-	    }
+	  int i,err;
+
+	  if ((err = evaluate(A,opt->fv,opt->fc,opt->field)) != ERROR_OK)
+	    return err;
 	  
 	  ellipse_t e;
 	  
