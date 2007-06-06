a75758666bfb348a1900c7446bc0b65e4db17600
J.J. Green,,, <j.j.green@gmx.co.uk>
2007-06-06 22:39:57 +0000

    *** empty log message ***

--- src/libvfplot/ellipse.c
+++ src/libvfplot/ellipse.c
@@ -2,13 +2,15 @@
   ellipse.c
   ellipse structures, and geometric queries on them
   J.J.Green 2007
-  $Id: ellipse.c,v 1.5 2007/06/05 09:29:31 jjg Exp jjg $
+  $Id: ellipse.c,v 1.6 2007/06/05 22:32:59 jjg Exp jjg $
 */
 
 #include <math.h>
 
 #include <vfplot/ellipse.h>
 #include <vfplot/matrix.h>
+#include <vfplot/cubic.h>
+#include <vfplot/polynomial.h>
 
 /* find point on an ellipse which are tangent to a line angle t */
 
@@ -49,7 +51,7 @@ extern int ellipse_tangent_points(ellipse_t e,double t,vector_t* v)
 /*
   return the algebraic representation of an ellipse - these
   values obtained by rotating and translating the equation
-  for a centred and axis-aligned ellipse
+  for a centred and axis-aligned ellipse.
 */
 
 extern algebraic_t ellipse_algebraic(ellipse_t e)
@@ -89,12 +91,61 @@ static double algebraic_eval(vector_t v, algebraic_t a)
 /*
   returns whether the two ellipses intersect - for reasons
   of efficiency it does not check whether one is contained
-  within the other
+  within the other (that should be done elsewhere)
 */
 
-extern int ellipse_intersect(algebraic_t a1,algebraic_t a2)
+// #define DEBUG
+
+extern int ellipse_intersect(algebraic_t a,algebraic_t b)
 {
-  
+  int i;
+
+  /* the Bezout determinant R */
+
+  double 
+    v0  = a.A*b.B - b.A*a.B,
+    v1  = a.A*b.C - b.A*a.C,
+    v2  = a.A*b.D - b.A*a.D,
+    v3  = a.A*b.E - b.A*a.E,
+    v4  = a.A*b.F - b.A*a.F,
+    v5  = a.B*b.C - b.B*a.C,
+    v6  = a.B*b.E - b.B*a.E,
+    v7  = a.B*b.F - b.B*a.F,
+    v8  = a.C*b.D - b.C*a.D,
+    v9  = a.D*b.E - b.D*a.E,
+    v10 = a.D*b.F - b.D*a.F;
+
+  double 
+    R[5] = {
+      v2*v10 - v4*v4,
+      v0*v10 + v2*(v7+v9) - 2*v3*v4,
+      v0*(v7+v9) + v2*(v6-v8) - v3*v3 - 2*v1*v4,
+      v0*(v6-v8) + v2*v5 - 2*v1*v3,
+      v0*v5 - v1*v1
+    };
+
+  /* 
+     if R has a real root the ellipses intersect: check by
+     - coerceing leading coefficient positive 
+     - find roots of derivative 
+     - check that R is positive for those roots
+  */
+
+  if (R[4] < 0) for (i=0 ; i<5 ; i++) R[i] *= -1;
+
+  double dR[4] = {R[1],2*R[2],3*R[3],4*R[4]};
+  double rts[3];
+
+  int n = cubic_roots(dR,rts);
+
+#ifdef DEBUG
+  for (i=0 ; i<5 ; i++) printf("  %f\n",R[i]); 
+  printf("\n"); 
+  for (i=0 ; i<n ; i++) printf("  %f -> %f\n",rts[i],poly_eval(R,5,rts[i])); 
+  printf("\n"); 
+#endif
+
+  for (i=0 ; i<n ; i++) if (poly_eval(R,5,rts[i]) < 0) return 1; 
 
   return 0;
 }
@@ -121,7 +172,7 @@ extern int ellipse_vector_inside(vector_t v,algebraic_t e)
 
 int main(void)
 {
-  ellipse_t e;
+  ellipse_t e,f;
 
   e.centre.x = 1;
   e.centre.y = 0;
@@ -129,7 +180,7 @@ int main(void)
   e.minor    = 1;
   e.theta    = M_PI/2;
 
-  int i,n=8;
+  int i,n = 8;
 
   vector_t v[2];
 
@@ -152,6 +203,24 @@ int main(void)
 	       v[j].x,
 	       v[j].y,
 	       algebraic_eval(v[j],a));
+    }
+
+  int m = 1000;
+
+  f.centre.y = 0;
+  f.major    = 2;
+  f.minor    = 1;
+  f.theta    = 0;
+
+  for (i=0 ; i<m ; i++)
+    {
+      double x = i/200.0;
+
+      f.centre.x = x;
+
+      algebraic_t b = ellipse_algebraic(f);
+
+      printf("%f %s\n",x,(ellipse_intersect(a,b) ? "yes" : "no"));
     }
 
   return 0;
