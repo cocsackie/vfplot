cfce5f85a76f860450aa041dbcc80d8d05068d12
J.J. Green,,, <j.j.green@gmx.co.uk>
2007-06-14 22:03:12 +0000

    *** empty log message ***

--- src/testsuite/polynomial.c
+++ src/testsuite/polynomial.c
@@ -1,13 +1,13 @@
 /*
   cunit tests for polynomial.c
   J.J.Green 2007
-  $Id$
+  $Id: polynomial.c,v 1.1 2007/06/14 20:40:45 jjg Exp jjg $
 */
 
 #include <vfplot/polynomial.h>
 #include "polynomial.h"
 
-#define EVAL_TEST(p,x,y) CU_ASSERT_DOUBLE_EQUAL(y,poly_eval(p,5,x),eps);
+#define EVAL_TEST(p,x,y) CU_ASSERT_DOUBLE_EQUAL(poly_eval(p,5,x),y,eps);
 
 CU_TestInfo tests_polynomial[] = 
   {
@@ -21,6 +21,7 @@ extern void test_poly_eval(void)
 
   double p[6] = {0,0,0,0,0,1};
 
+  EVAL_TEST(p, 0.0, 0.0);
   EVAL_TEST(p, 1.0, 1.0);
   EVAL_TEST(p,-1.0,-1.0);
   EVAL_TEST(p, 2.0,32.0);
@@ -29,5 +30,6 @@ extern void test_poly_eval(void)
 
   EVAL_TEST(q, 0.0, 1.0);
   EVAL_TEST(q, 1.0, 6.0);
+  EVAL_TEST(q,-1.0, 0.0);
   EVAL_TEST(q, 2.0,63.0);
 }
