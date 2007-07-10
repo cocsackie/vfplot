4738adf10cf1c0309f54eef0332e84a33f094931
J.J. Green,,, <j.j.green@gmx.co.uk>
2007-07-10 22:44:01 +0000

    *** empty log message ***

--- src/testsuite/margin.h
+++ src/testsuite/margin.h
@@ -1,10 +1,10 @@
 /*
   polynomial.h
   J.J.Green 2007
-  $Id: cubic.h,v 1.1 2007/06/14 23:21:54 jjg Exp $
+  $Id: margin.h,v 1.1 2007/07/10 22:17:00 jjg Exp jjg $
 */
 
 #include <CUnit/CUnit.h>
 
-extern CU_TestInfo tests_cubic[]; 
-extern void test_cubic_roots(void);
+extern CU_TestInfo tests_margin[]; 
+extern void test_margin(void);
