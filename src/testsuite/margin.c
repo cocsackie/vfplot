c6783f9fa43a52363c84f43d73f1a0f440cab226
J.J. Green,,, <j.j.green@gmx.co.uk>
2007-07-10 22:33:48 +0000

    Initial revision

---
+++ src/testsuite/margin.c
@@ -0,0 +1,29 @@
+/*
+  cunit tests for cubic.c
+  J.J.Green 2007
+  $Id: cubic.c,v 1.2 2007/06/16 00:34:20 jjg Exp $
+*/
+
+#include <vfplot/margin.h>
+#include "margin.h"
+
+CU_TestInfo tests_margin[] = 
+  {
+    {"margin function evaluate",test_margin},
+    CU_TEST_INFO_NULL,
+  };
+
+extern void test_margin(void)
+{
+  double eps = 1e-10;
+
+  CU_ASSERT_DOUBLE_EQUAL(margin(0,1,1),1.00,eps);
+  CU_ASSERT_DOUBLE_EQUAL(margin(1,1,1),1.25,eps);
+  CU_ASSERT_DOUBLE_EQUAL(margin(2,1,1),2.00,eps);
+  CU_ASSERT_DOUBLE_EQUAL(margin(3,1,1),3.00,eps);
+
+  CU_ASSERT_DOUBLE_EQUAL(margin(0,2,1),2.0  ,eps);
+  CU_ASSERT_DOUBLE_EQUAL(margin(1,2,1),2.125,eps);
+  CU_ASSERT_DOUBLE_EQUAL(margin(2,2,1),2.5  ,eps);
+  CU_ASSERT_DOUBLE_EQUAL(margin(3,2,1),3.125,eps);
+}
