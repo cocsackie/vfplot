ed469adaa13ee433a068c4a25d541b4c490fc031
J.J. Green,,, <j.j.green@gmx.co.uk>
2007-07-19 22:35:08 +0000

    Initial revision

---
+++ src/libvfplot/dim2.c
@@ -0,0 +1,21 @@
+/*
+  dim2.c
+  vfplot adaptive plot, dimension 2
+  J.J.Green 2007
+  $Id: dim1.c,v 1.1 2007/07/17 21:24:02 jjg Exp $
+*/
+
+#include <math.h>
+#include <stdlib.h>
+
+#include <vfplot/dim2.h>
+
+#include <vfplot/error.h>
+#include <vfplot/evaluate.h>
+
+extern int dim2(domain_t* dom,int* n,arrow_t** pA)
+{
+  return ERROR_OK;
+}
+
+
---
+++ src/libvfplot/dim2.h
@@ -0,0 +1,16 @@
+/*
+  dim2.h 
+  vfplot adaptive at dimension 2
+  J.J.Green 2007
+  $Id: dim1.h,v 1.1 2007/07/17 21:23:59 jjg Exp $
+*/
+
+#ifndef DIM2_H
+#define DIM2_H
+
+#include <vfplot/domain.h>
+#include <vfplot/arrow.h>
+
+extern int dim2(domain_t*,int*,arrow_t**);
+
+#endif
