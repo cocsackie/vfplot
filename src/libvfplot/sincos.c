6e0f93b79ee235f0ac253fc5f831a8a2e9aae57c
J.J. Green,,, <j.j.green@gmx.co.uk>
2007-08-02 21:24:37 +0000

    Initial revision

---
+++ src/libvfplot/sincos.c
@@ -0,0 +1,22 @@
+/*
+  sincos.c
+  conditonal sincos
+  J.J.Green 2007
+  $Id$
+*/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifndef HAVE_SINCOS
+
+#include <math.h>
+
+extern void sincos(double t,double *s,double *c)
+{
+  *s = sin(t);
+  *c = cos(t);
+}
+
+#endif
