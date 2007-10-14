73b069ed483fe567edec16dd75a6cbeb23ec3676
J.J. Green,,, <j.j.green@gmx.co.uk>
2007-10-14 19:10:28 +0000

    *** empty log message ***

--- src/vfplot/field.c
+++ src/vfplot/field.c
@@ -6,7 +6,7 @@
   to store the (signed) curvature of the field
 
   J.J.Green 2007
-  $Id: field.c,v 1.5 2007/10/07 18:41:05 jjg Exp jjg $ 
+  $Id: field.c,v 1.6 2007/10/09 21:05:22 jjg Exp jjg $ 
 */
 
 #ifdef HAVE_CONFIG_H
@@ -16,6 +16,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 #ifdef HAVE_LIBNETCDF
 #include <netcdf.h>
@@ -80,7 +81,7 @@ extern field_t* field_read(format_t format,int n,char** file)
     {
     case format_auto:
       // FIXME
-      fprintf(stderr,"not implementted yet\n");
+      fprintf(stderr,"not implemented yet\n");
       return NULL;
 
     case format_grd:
@@ -100,14 +101,15 @@ extern field_t* field_read(format_t format,int n,char** file)
 
   field->k = k;
 
-  // #define DUMP_CURVATURE
-
 #ifdef DUMP_CURVATURE
   bilinear_write("k.dat",k);
 #endif
 
   return field;
 }
+
+#define ORDER_XY 1
+#define ORDER_YX 2
 
 extern field_t* field_read_grd2(char* grdu,char* grdv)
 {
@@ -134,30 +136,7 @@ extern field_t* field_read_grd2(char* grdu,char* grdv)
     {
       int j;
 
-      /* x,y dimension ids and lengths */
-
-      int did[2];
-      char *dnm[2] = {"x","y"}; 
-      size_t dln[2];
-
-      for (j=0 ; j<2 ; j++)
-	{
-	  if ((err = nc_inq_dimid(ncid[i],dnm[j],did+j)) != NC_NOERR)
-	    {
-	      fprintf(stderr, "error getting dimension %s id from %s : %s\n",
-		      dnm[j],grd[i],nc_strerror(err));
-	      return NULL;
-	    }
-
-	  if ((err = nc_inq_dimlen(ncid[i],did[j],dln + j))!= NC_NOERR)
-	    {
-	      fprintf(stderr, "error getting dimension %s length from %s : %s\n",
-		      dnm[j],grd[i],nc_strerror(err));
-	      return NULL;
-	    }
-	}
-
-      /* z id */
+      /* z variable id */
 
       int vid;
 
@@ -165,51 +144,152 @@ extern field_t* field_read_grd2(char* grdu,char* grdv)
 	{
 	  fprintf(stderr, "error getting z id from %s : %s\n",grd[i],
 		  nc_strerror(err));
-	      return NULL;
+	  return NULL;
 	}
 
-      /* read x,y arrays */
+      /* get the variable's dimensions */
 
-      double x[dln[0]],y[dln[1]];
+      int ndims;
 
-      if ((err = nc_get_var_double(ncid[i],did[0],x)) != NC_NOERR)
+      if ((err = nc_inq_varndims(ncid[i],vid,&ndims)) != NC_NOERR)
 	{
-	  fprintf(stderr, "error getting x from %s : %s\n",grd[i],
+	  fprintf(stderr, "error getting number of dimensions from %s : %s\n",grd[i],
 		  nc_strerror(err));
-	      return NULL;
+	  return NULL;
 	}
 
-      if ((err = nc_get_var_double(ncid[i],did[1],y)) != NC_NOERR)
+      if (ndims != 2)
 	{
-	  fprintf(stderr, "error getting y from %s : %s\n",grd[i],
-		  nc_strerror(err));
-	      return NULL;
+	  fprintf(stderr, "strange dimensions from %s (%i)\n",grd[i],ndims);
+	  return NULL;
 	}
+
+      int dimid[2];
+
+      if ((err = nc_inq_vardimid(ncid[i],vid,dimid)) != NC_NOERR)
+	{
+	  fprintf(stderr, "error getting dimensions ids from %s : %s\n",grd[i],
+		  nc_strerror(err));
+	  return NULL;
+	}
+
+#ifdef FIELD_READ_DEBUG
+      printf("z id %i, dimensions %i, %i\n",vid,dimid[0],dimid[1]);
+#endif
+      size_t dlen[2];
+      char name[2][NC_MAX_NAME];
+
+      for (j=0 ; j<2 ; j++)
+	{
+	  if ((err = nc_inq_dim(ncid[i],dimid[j],name[j],dlen+j)) != NC_NOERR)
+	    {
+	      fprintf(stderr, "error getting dimension %i details : %s\n",
+		      j,nc_strerror(err));
+	      return NULL;
+	    }
+	}
+
+#ifdef FIELD_READ_DEBUG
+      printf("lengths %i, %i\n",dlen[0],dlen[1]);
+#endif
+
+      /*
+	GMT grd files usually have the dimensions of z being (y,x) but I don't
+	know if this is always the case -- so here we determine the order and
+	later we switch the insert order
+      */
+
+      int order;
+
+      if (strcmp("x",name[0]) == 0)
+	{
+	  if (strcmp("y",name[1]) == 0)
+	    {
+	      order = ORDER_XY;
+	    }
+	  else
+	    {
+	      fprintf(stderr, "netcdf file has no y dimension (%s instead)\n",name[1]);
+	      return NULL;
+	    }
+	}
+      else if (strcmp("y",name[0]) == 0)
+	{
+	  if (strcmp("x",name[1]) == 0)
+	    {
+	      order = ORDER_YX;
+	    }
+	  else
+	    {
+	      fprintf(stderr, "netcdf file has no x dimension (%s instead)\n",name[1]);
+	      return NULL;
+	    }
+	}
+      else
+	{
+	  fprintf(stderr, "netcdf file has no x or y dimension (%s, %s instead)\n",
+		  name[0],name[1]);
+	  return NULL;
+	}
+
+#ifdef FIELD_READ_DEBUG
+      printf("order is %s\n",(order == ORDER_XY ? "xy" : "yx"));
+#endif
+
+      /* read dimensional extrema for bounding box */
+
+      double dmin[2],dmax[2];
+
+      for (j=0 ; j<2 ; j++)
+	{
+	  size_t minidx = 0, maxidx = dlen[j] - 1;
+	  
+	  if ((err = nc_get_var1_double(ncid[i],dimid[j],&minidx,dmin+j)) != NC_NOERR)
+	    {
+	      fprintf(stderr, "error getting dim %i min from %s : %s\n",j,grd[i],
+		      nc_strerror(err));
+	      return NULL;
+	    }
+
+	  if ((err = nc_get_var1_double(ncid[i],dimid[j],&maxidx,dmax+j)) != NC_NOERR)
+	    {
+	      fprintf(stderr, "error getting dim %i max from %s : %s\n",j,grd[i],
+		      nc_strerror(err));
+	      return NULL;
+	    }
+	}
+
+#ifdef FIELD_READ_DEBUG
+      printf("bbox %f %f, %f %f\n",dmin[0],dmax[0],dmin[1],dmax[1]);
+#endif
 
       /* create bilinear interpolant */
 
-      bbox_t bb = {{x[0],x[dln[0]-1]},
-		   {y[0],y[dln[1]-1]}};
-
       B[i] = bilinear_new();
 
-      bilinear_dimension(dln[0],dln[1],bb,B[i]);
+      if (order == ORDER_XY)
+	{
+	  bbox_t bb = {{dmin[0],dmax[0]},
+		       {dmin[1],dmax[1]}};
 
-      for (j=0 ; j<dln[0] ; j++)
+	  bilinear_dimension(dlen[0],dlen[1],bb,B[i]);
+	}
+      else
+	{
+	  bbox_t bb = {{dmin[1],dmax[1]},
+		       {dmin[0],dmax[0]}};
+
+	  bilinear_dimension(dlen[1],dlen[0],bb,B[i]);
+	}
+
+      for (j=0 ; j<dlen[0] ; j++)
 	{
 	  int k;
 
-	  for (k=0 ; k<dln[1] ; k++)
+	  for (k=0 ; k<dlen[1] ; k++)
 	    {
-
-	      /*
-		FIXME - the order of idx[] is in the file, use
-		nc_inq_varndims() nc_inq_vardimid() to extract 
-		them
-	      */
-
 	      double z;
-	      size_t idx[2] = {k,j};
+	      size_t idx[2] = {j,k};
 
 	      if ((err = nc_get_var1_double(ncid[i],vid,idx,&z)) != NC_NOERR)
 		{
@@ -218,7 +298,8 @@ extern field_t* field_read_grd2(char* grdu,char* grdv)
 		  return NULL;
 		}
 
-	      bilinear_setz(j,k,z,B[i]);
+	      if (order == ORDER_XY) bilinear_setz(j,k,z,B[i]);
+	      else bilinear_setz(k,j,z,B[i]);
 	    }
 	}
 
