2285d380a962b3dc98d1c4058dc3593fbac242c4
J.J. Green,,, <j.j.green@gmx.co.uk>
2007-10-15 22:19:48 +0000

    negative values of m handled

--- src/libvfplot/margin.c
+++ src/libvfplot/margin.c
@@ -3,7 +3,7 @@
 
   margin function with natural parameters
   J.J.Green 2007
-  $Id: margin.c,v 1.1 2007/07/10 22:14:30 jjg Exp jjg $
+  $Id: margin.c,v 1.2 2007/07/10 22:34:22 jjg Exp jjg $
 */
 
 #include <math.h>
@@ -13,14 +13,20 @@
 
 /*
   this is a qudratic spline through (0,b) spliced 
-  with y=mx 
+  with y=mx if m>0, or a linear ramp down to L*b
+  if m<0
 
   m^2 x^2
-  ------- + b  (for 0<x<2b/m)
+  ------- + b  (for 0<x<2b/m, m>0)
     4b
 
-  mx           (for 2b/m<x)
+  mx           (for 2b/m<x, m>0)
 
+  mx + b       (for 0<x<(L-1)b/m, m<0)
+
+  Lb           (for (L-1)b/m<x, m<0)
+
+  the linear ramp could be made into a proper spline
 */
 
 extern double margin(double x,double b,double m)
@@ -33,6 +39,11 @@ extern double margin(double x,double b,double m)
   
   double mx = m*x;
 
-  return (mx < 2.0*b ? pow(mx,2.0)/(4.0*b) + b : mx);
+  if (m>0.0)
+    return (mx < 2.0*b ? pow(mx,2.0)/(4.0*b) + b : mx);
+  else
+    return (mx > (MARGIN_RAMP_FACTOR-1.0)*b 
+	    ? b + mx 
+	    : MARGIN_RAMP_FACTOR*b);
 }
 
