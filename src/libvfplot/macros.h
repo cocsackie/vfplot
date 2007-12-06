ff00c363aaa575c78a7da89455d6c636000fea6f
J.J. Green,,, <j.j.green@gmx.co.uk>
2007-12-06 00:18:51 +0000

    Initial revision

---
+++ src/libvfplot/macros.h
@@ -0,0 +1,16 @@
+/*
+  macros.h
+
+  a few utility preprocessor macros
+
+  J.J.Green 2007
+  $Id$
+*/
+
+#ifndef MACROS_H
+#define MACROS_H
+
+#define MAX(a,b) ((a)>(b) ? (a) : (b))
+#define MIN(a,b) ((a)<(b) ? (a) : (b))
+
+#endif
