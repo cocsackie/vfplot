770079ad15c5dcfdcba7bdc500cecff8a2dfbeec
J.J. Green,,, <j.j.green@gmx.co.uk>
2008-01-03 00:24:39 +0000

    Initial revision

---
+++ src/libvfplot/constants.h
@@ -0,0 +1,15 @@
+/*
+  defines some common but non-statndard constants
+
+  J.J.Green 2007
+  $Id$
+*/
+
+#ifndef CONSTANTS_H 
+#define CONSTANTS_H 
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#endif
