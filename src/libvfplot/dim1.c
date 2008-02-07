814fc660fa96777b5c934494fef11b54d3121687
J.J. Green,,, <j.j.green@gmx.co.uk>
2008-02-07 23:40:05 +0000

    *** empty log message ***

--- src/libvfplot/dim1.c
+++ src/libvfplot/dim1.c
@@ -2,7 +2,7 @@
   dim1.c
   vfplot adaptive plot, dimension 1 
   J.J.Green 2007
-  $Id: dim1.c,v 1.8 2008/01/04 00:35:17 jjg Exp jjg $
+  $Id: dim1.c,v 1.9 2008/02/06 23:55:45 jjg Exp jjg $
 */
 
 #ifdef HAVE_CONFIG_H
@@ -25,7 +25,7 @@
 
 /* number of iterations in dim-1 placement */
 
-#define DIM1_POS_ITER  4
+#define DIM1_POS_ITER 8
 
 /* maximum number of arrows on a boundary segment */
 
@@ -38,6 +38,18 @@
 
 #define DIM1_SLACK_ITER 8
 #define DIM1_SLACK_MIN  0.05
+
+/*
+  when placing the next ellipse along the boundary
+  we initially shift by the ellipse diameter mutiplied 
+  by these min ... max values
+*/
+
+#define DIM1_SHIFT_MIN  1.0 
+#define DIM1_SHIFT_MAX  2.0
+#define DIM1_SHIFT_N    8
+
+#define DIM1_SHIFT_STEP ((DIM1_SHIFT_MAX - DIM1_SHIFT_MIN)/DIM1_SHIFT_N)
 
 /*
   if the end ellipses have a pw distance is less 
@@ -53,11 +65,11 @@
    the boundary line
 */
 
-#define DIM1_EPROJ_ITER 6
+#define DIM1_EPROJ_ITER 8
 
 /*
   perform the dimension-1 processing - for each
-  pair of points, place as many glyphs as possible
+  pair of points, place as many arrows as possible
   between them.
 
   we repeatedly place ellipses with their left
@@ -105,7 +117,6 @@ static int alist_dim1(alist_t* a,dim1_opt_t *opt)
   if appropriate (which is why we return it)
 */
 
-static double contact_angle(ellipse_t,double);
 static int project_ellipse(vector_t,vector_t,vector_t,mt_t,ellipse_t*);
 
 static alist_t* dim1_edge(alist_t *La, alist_t *Lb,dim1_opt_t opt)
@@ -115,19 +126,19 @@ static alist_t* dim1_edge(alist_t *La, alist_t *Lb,dim1_opt_t opt)
   arrow_t Aa = La->arrow, Ab = Lb->arrow;
   vector_t pa = La->v, pb = Lb->v;
   vector_t seg = vsub(pb,pa);
-  double lseg = vabs(seg);
+  double len = vabs(seg);
   vector_t v = vunit(seg);
-  double psi = vang(v), xi = psi - M_PI/2.0; 
+  double psi = vang(v); 
 
   alist_t *Lc = La;
 
   /* initialise A[] */
 
+  A[0] = Aa;
+
   int k = 1;
 
-  A[k-1] = Aa;
-
-  /* get ellipses */
+  /* extremal ellipses */
 
   ellipse_t Ea,Eb;
 
@@ -138,15 +149,13 @@ static alist_t* dim1_edge(alist_t *La, alist_t *Lb,dim1_opt_t opt)
 
   double pwseg = sqrt(contact(Ea,Eb));
 
-  if ((Ea.minor + Eb.minor > lseg) || (pwseg < DIM1_PW_MIN)) 
+  if ((Ea.minor + Eb.minor > len) || (pwseg < DIM1_PW_MIN)) 
     goto output;
 
-  /* loop variables (spurious icc warning that these are not initialised) */
-
-#if 1
-
-  arrow_t   A1, A2;
+  arrow_t   A1;
   ellipse_t E1, E2;
+
+  /* E1 should intesect Ea */
 
   double mu = projline(pa,v,Ea.centre);
 
@@ -154,228 +163,158 @@ static alist_t* dim1_edge(alist_t *La, alist_t *Lb,dim1_opt_t opt)
     {
       vector_t x0 = vsub(Ea.centre,smul(mu,v));
 
-      //printf("p (%f,%f) %f %f\n",pa.x,pa.y,x0.x,x0.y);
-
       project_ellipse(pa,v,x0,opt.mt,&E1);
 
-      A[1].centre = vadd(A[0].centre,vsub(E1.centre,Ea.centre));
+      A1.centre = vadd(Aa.centre,vsub(E1.centre,Ea.centre));
 
-      evaluate(A+1);
+      evaluate(&A1);
 
-      k++;
+      if (!ellipse_intersect(Ea,E1))
+	{
+	  /* handle this case FIXME */
+
+	  fprintf(stderr,"lower bracket fail at (%f,%f)\n",
+		  pa.x,pa.y);
+
+	  A[k++] = A1;
+
+	  goto output;
+	}
     }
   else
     {
+      A1 = Aa;
       E1 = Ea;
     }
 
-  //printf("%g\n",mu);
+  /* 
+     E2 should not intesect Ea -- we make the guess
+     that if E2 is adjacent to E1 then this will be 
+     that case (untrue for pathological examples) 
+     and that E2 is similar to E1. If that fails we
+     try a bit further along
+  */
+  
+  int isect;
 
-#else
-
-  arrow_t   A1 = Aa, A2;
-  ellipse_t E1 = Ea, E2;
-
-  /* intitial contact angle */
-
-  xi = contact_angle(E1,psi);
-
-  // printf("(%f,%f) -> (%f,%f)\n",pa.x,pa.y,E1.centre.x,E1.centre.y);
-
-  /* initial tangent points */
-
-  vector_t tph1[2], tpv1[2], tph2[2], tpv2[2];
-
-  ellipse_tangent_points(E1,xi,tph1);
-  ellipse_tangent_points(E1,psi,tpv1);
-
-  /* place the ellipses */
-
-  static int warned = 0;
-
-  do 
+  for (isect=1,i=0 ; (i<DIM1_SHIFT_N) && isect ; i++)
     {
-      /* initial guess for E2 */
+      double w = (DIM1_SHIFT_MIN + DIM1_SHIFT_STEP*i) * 
+	2.0 * ellipse_radius(E1,E1.theta - psi);
 
-      double d = 2.0*(fabs((E1.major - E1.minor)*sin(E1.theta)) + E1.minor);
+      project_ellipse(pa,v,vadd(E1.centre,smul(w,v)),opt.mt,&E2);
+  
+      isect = ellipse_intersect(E2,Ea);
+    }
 
-      E2.centre = vadd(A1.centre,smul(d,v));
+  if (isect)
+    {
+      fprintf(stderr,"upper bracket fail at (%f,%f)\n",
+	      pa.x,pa.y);
+    }
 
-      /* 
-	 this a bit obscure - we need to determine which of the 
-	 tangent points are at the top and left of E1, relative
-	 to the vector v in the segment direction 
-      */
+  /* now the bracketing to obtain touching ellipse Et */
+
+  ellipse_t Et;
+
+  for (i=0 ; i<DIM1_POS_ITER ; i++)
+    {
+      project_ellipse(pa,v,vmid(E1.centre,E2.centre),opt.mt,&Et);
+
+      if (ellipse_intersect(Et,Ea)) E1 = Et;
+      else E2 = Et;
+    }
+
+  if (ellipse_intersect(Et,Eb)) goto output;
       
-      vector_t 
-	tplft1 = tpv1[bend_2v(v,vsub(tpv1[0],tpv1[1])) == rightward], 
-	tptop1 = tph1[bend_3pt(tplft1,tph1[0],tph1[1]) == leftward],
-	v1 = intersect(tplft1,tptop1,psi,xi);
+  A[k].centre = vadd(Aa.centre,vsub(Et.centre,Ea.centre));
+  evaluate(A+k);
+  k++;
+  
+  /* 
+     now more or less the same, but fitting E[k+1] next to E[k]
+     rather than E[1] next to Ea -- this should probably be
+     abstracted out into a function
+  */
 
-      /* iterate to place E2 */
+  ellipse_t Ep = Et;
 
+  for (i=0 ; i<DIM1_MAX_ARROWS ; i++)
+    {
       int j;
-      
+
+      E1 = Ep;
+
+      for (isect=1,j=0 ; (j<DIM1_SHIFT_N) && isect ; j++)
+	{
+	  double w = (DIM1_SHIFT_MIN + DIM1_SHIFT_STEP*j) * 
+	    2.0 * ellipse_radius(E1,E1.theta - psi);
+
+	  project_ellipse(pa,v,vadd(E1.centre,smul(w,v)),opt.mt,&E2);
+
+	  isect = ellipse_intersect(E2,Ep);
+	}
+
+      if (isect)
+	{
+	  fprintf(stderr,"upper bracket fail at ellipse %i (%f,%f)\n",
+		  i,E1.centre.x,E1.centre.y);
+	  goto output;
+	}
+
       for (j=0 ; j<DIM1_POS_ITER ; j++)
 	{
-	  m2_t M;
-
-	  /* 
-	     in the case that thers is no room between
-	     the end ellipses (ie, they are close) then
-	     we may find that the metric tensor is not defined -- 
-	     the putative middle ellipse is outside the domain.
-	     Hence the earlier check for the pw distance.
-	  */
-
-	  switch (metric_tensor(E2.centre,opt.mt,&M))
-	    {
-	      double edr;
-
-	    case ERROR_OK: break;
-	    case ERROR_NODATA:
-
-	      fprintf(stderr,"edge truncated (bad mt) at node %i (pw %3f)\n",k,pwseg);
-
-	      /*
-		this can happen if the distance of the ellipse's
-		centre to the boundary is less than the width of
-		a cell in the bilinear representation of the metric
-		tensor (this will usually happen, the tensor is 
-		only known in the interior). We check and warn
-		(but only the once).
-	      */
-
-	      if ((!warned) && ((edr = mt_edge_granular(opt.mt,E2.centre)) < 1.0))
-		{
-		  fprintf(stderr,"grid granularity problem?\n");
-		  fprintf(stderr,"point (%.3f,%.3f) is %.3f pixels from the boundary\n",
-			  E2.centre.x,
-			  E2.centre.y,
-			  edr);
-		  fprintf(stderr,"try increasing the scale or metric-tensor grid-size\n");
-		  warned = 1;
-		}
-
-	      /* bilinear_write("bilinear.dat",opt.mt.a); */
-
-	      goto output;
-
-	    default: return NULL;
-	    }
-
-	  if (mt_ellipse(M,&E2) != 0)
-	    {
-	      fprintf(stderr,"bad metric tensor %f %f %f %f\n",M.a,M.b,M.c,M.d);
-	      return NULL;
-	    }
-
-	  ellipse_tangent_points(E2,xi,tph2);
-	  ellipse_tangent_points(E2,psi,tpv2);
-
-	  /* 
-	     again wth the obscurity, now need to find which of the 
-	     tangent points are at the bottom and left of E2
-	  */
-
-	  vector_t
-	    tplft2 = tpv2[bend_2v(v,vsub(tpv2[0],tpv2[1])) == rightward], 
-	    tpbot2 = tph2[bend_3pt(tplft2,tph2[0],tph2[1]) == rightward],
-	    v2 = intersect(tplft2,tpbot2,psi,xi);
-
-	  /* move E2 to where it should be */
+	  project_ellipse(pa,v,vmid(E1.centre,E2.centre),opt.mt,&Et);
 	  
-	  E2.centre = vadd(E2.centre,vsub(v1,v2));
-
-	  /* adjust contact angle */
-
-	  xi = contact_angle(E2,psi);
+	  if (ellipse_intersect(Et,Ep)) E1 = Et;
+	  else E2 = Et;
 	}
+      
+      if (ellipse_intersect(Et,Eb)) goto output;
+      
+      A[k].centre = vadd(A[k-1].centre,vsub(Et.centre,Ep.centre));
+      evaluate(A+k);
+      k++;
 
-      /* done if we intersect the b ellipse */
-
-      if (ellipse_intersect(E2,Eb)) goto output;
-
-      /* otherwise add the arrow at this ellipse */
-
-      A2.centre = E2.centre;
-
-      switch (evaluate(&A2))
-	{
-	case ERROR_OK: break;
-	case ERROR_NODATA: 
-	  fprintf(stderr,"edge truncated (evaluate) at node %i\n",k);
-	  goto output;
-	default: return NULL;
-	}
-
-      A[k++] = A2;
-
-      /* reuse in the next loop */
-
-      A1 = A2;
-      E1 = E2;
-
-      for (i=0 ; i<2 ; i++)
-	{
-	  tph1[i] = tph2[i];
-	  tpv1[i] = tpv2[i];
-	}
+      Ep = Et;
     }
-  while (k<DIM1_MAX_ARROWS);
-
-#endif
-
+  
+  fprintf(stderr,"too many arrows on one segment (%i)\n",DIM1_MAX_ARROWS);
+   
   /* goto considered groovy */
-
+      
  output:
-
-#if 0
-  printf("%i\n",k);
-#endif
 
   /* share out the slack */
 
   if (k>2)
     {
       double slack;
+      double w = 2.0*ellipse_radius(Ep,Ep.theta-psi);
 
-      /* Ek is the last ellipse of the pack */
+      ellipse_t E = Ep;
 
-      ellipse_t Ek = E1;
-
-      /* 
-	 mid-width of Ek, which is r = r(theta) in polar
-	 represtentaton of the ellipse
-      */
-
-      double ekmw = 2.0*ellipse_radius(Ek,Ek.theta);
-
-      /* bracketing iteration to find slack */
-
-      ellipse_t E = Ek;
-
-      E.centre = vadd(Ek.centre,smul(ekmw,v));
+      E.centre = vadd(Ep.centre,smul(w,v));
 
       if (ellipse_intersect(E,Eb))
 	{
-	  double smin = 0.0, smax = ekmw;
+	  double smin = 0.0, smax = w;
 
 	  for (i=0 ; i<DIM1_SLACK_ITER ; i++)
 	    {
 	      slack = (smin+smax)/2.0;
 
-	      E.centre = vadd(Ek.centre,smul(slack,v));
+	      E.centre = vadd(Ep.centre,smul(slack,v));
 
 	      if (ellipse_intersect(E,Eb)) smax = slack;
 	      else smin = slack;
 	    }
 	}
-      else slack = ekmw;
+      else slack = w;
 
       /* if there's enough to share then do so */
 
-      if (slack > DIM1_SLACK_MIN*ekmw)
+      if (slack > DIM1_SLACK_MIN*w)
 	for (i=1 ; i<k ; i++)
 	  A[i].centre = vadd(A[i].centre,smul(i*slack/k,v));
     }
@@ -408,7 +347,6 @@ static alist_t* dim1_edge(alist_t *La, alist_t *Lb,dim1_opt_t opt)
 
 static int project_ellipse(vector_t p, vector_t v, vector_t x, mt_t mt, ellipse_t* pE)
 {
-  int err;
   size_t i;
   ellipse_t E;
   m2_t M;
@@ -437,8 +375,6 @@ static int project_ellipse(vector_t p, vector_t v, vector_t x, mt_t mt, ellipse_
       j = ((vdet(v,q[0]) < vdet(v,q[1])) ? 0 : 1);
       
       x = vadd(x,q[j]);
-
-      // printf("%f %f\n",x.x,x.y);
     }
 
   pE->centre = x;
@@ -446,16 +382,5 @@ static int project_ellipse(vector_t p, vector_t v, vector_t x, mt_t mt, ellipse_
   mt_ellipse(M,pE);
 
   return ERROR_OK;
-}
-
-/* 
-   evaluate the contact angle xi between adjacent ellipses
-   on the boundary at angle psi - this is a bid fiddly to
-   describe, see the implemenation notes on contact angles
-*/
-
-static double contact_angle(ellipse_t E,double psi)
-{
-  return E.theta-atan(E.minor/(E.major*tan(psi-E.theta)));
 }
 
