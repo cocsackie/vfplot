c52ccd772a76936ad48a1069f3dccd465fc66ff3
J.J. Green,,, <j.j.green@gmx.co.uk>
2008-03-23 18:16:31 +0000

    *** empty log message ***

--- src/testsuite/bilinear.h
+++ src/testsuite/bilinear.h
@@ -1,7 +1,7 @@
 /*
   bilinear.h
   J.J.Green 2007
-  $Id: bilinear.h,v 1.1 2007/08/16 22:43:34 jjg Exp jjg $
+  $Id: bilinear.h,v 1.2 2007/09/24 21:32:45 jjg Exp jjg $
 */
 
 #include <CUnit/CUnit.h>
@@ -10,4 +10,4 @@ extern CU_TestInfo tests_bilinear[];
 extern void test_bilinear_quadratic(void);
 extern void test_bilinear_nodata(void);
 extern void test_bilinear_integrate(void);
-
+extern void test_bilinear_domain(void);
