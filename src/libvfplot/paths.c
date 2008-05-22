50d21a5a068276a661f44ff6d98720f1cb7e7a62
J.J. Green,,, <j.j.green@gmx.co.uk>
2008-05-22 22:11:07 +0000

    *** empty log message ***

--- src/libvfplot/paths.c
+++ src/libvfplot/paths.c
@@ -3,8 +3,10 @@
   structures for boundary paths of arrows
 
   J.J.Green 2008
-  $Id: paths.c,v 1.2 2008/05/20 21:59:16 jjg Exp jjg $
+  $Id: paths.c,v 1.3 2008/05/21 22:08:18 jjg Exp jjg $
 */
+
+#include <math.h>
 
 #include <vfplot/paths.h>
 #include <vfplot/graph.h>
@@ -70,20 +72,29 @@ extern int paths_decimate(gstack_t* paths,double d)
 
 /* 
    gstack iterator, acts on a single path
+
    - dump the corners into the cns array 
+
    - create graph of ellipse intersections with nodes
      weighted by the maximum length of their edges.
      thus boundary corner nodes are precious 
+
    - totally disconnect the graph in a greedy manner
      aimng to keep the weight high
+
    - push the non-deleted node ellipses back onto 
-     the stack 
+     the stack
+
+  here "intersect" means the pw-distance D is less 
+  than Dmin, and since D = sqrt(x), x = contact(),
+  it is the same to check x < xmin = Dmin^2
 */
 
 static int path_decimate(gstack_t** path, double* Dmin)
 {
   int i,n = gstack_size(*path);
-
+  double xmin = pow(*Dmin,2);
+  
   corner_t cns[n];
 
   /* 
@@ -116,27 +127,49 @@ static int path_decimate(gstack_t** path, double* Dmin)
 
   if (graph_init(n,&G) != 0) return -1; 
 
+  size_t err = 0;
+
   for (i=0 ; i<n-1 ; i++)
     {
       int j;
 
       for (j=i+1 ; j<n ; j++)
 	{
-	  double D = contact_mt(vsub(e[j],e[i]),mt[i],mt[j]);
+	  double x = contact_mt(vsub(e[j],e[i]),mt[i],mt[j]);
 
-	  if (D < *Dmin)
+	  /* 
+	     failed contact() results in edge not being
+	     included in the graph so possible intesection
+	     survives - save the number for a warning (below)
+	  */
+
+	  if (x<0)
+	    {
+	      err++;
+	      continue;
+	    }
+
+	  /* 
+	     weight of each node is the maximum of the
+	     contact-distances of the incoming edges
+	  */
+
+	  if (x < xmin)
 	    {
 	      double 
 		w1 = graph_get_weight(G,i),
 		w2 = graph_get_weight(G,j);
 
-	      graph_set_weight(G,i,MAX(w1,D));
-	      graph_set_weight(G,j,MAX(w2,D));
+	      graph_set_weight(G,i,MAX(w1,x));
+	      graph_set_weight(G,j,MAX(w2,x));
 
 	      if (graph_add_edge(G,i,j) != 0) return -1;
 	    }
 	}
     }
+
+  if (err)
+    fprintf(stderr,"failed contact distance for %i pairs\n",err);
 
   /* greedy node deletion to obtain non-intersecting subset */
 
