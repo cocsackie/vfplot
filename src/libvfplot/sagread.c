c4e237d90c5f6dbc3f23df33b3e71ad43efb9b8b
J.J. Green,,, <j.j.green@gmx.co.uk>
2008-05-27 23:09:32 +0000

    broken!

--- src/libvfplot/sagread.c
+++ src/libvfplot/sagread.c
@@ -4,7 +4,7 @@
   read simple ascii grid files
   J.J.Green 2008
 
-  $Id: sagread.c,v 1.1 2008/05/26 22:57:02 jjg Exp jjg $
+  $Id: sagread.c,v 1.2 2008/05/27 15:35:58 jjg Exp jjg $
 */
 
 #include <string.h>
@@ -15,7 +15,8 @@
 
 /* max length of a line in a SAG file */
 
-#define MAX_LINE 256
+#define MAX_HEADER_LINE 256
+#define MAX_DATA_LINE  1024
 
 /* maximal dimensions of grid and vectors */
 
@@ -36,6 +37,8 @@
   calls to get the data points
 */
 
+#define DELIM " \t"
+
 extern int sagread_open(const char* path,sagread_t* S)
 {
   FILE* st = fopen(path,"r");
@@ -46,9 +49,9 @@ extern int sagread_open(const char* path,sagread_t* S)
       return SAGREAD_ERROR;
     }
 
-  char line[MAX_LINE];
+  char line[MAX_HEADER_LINE];
 
-  if (! fgets(line,MAX_LINE,st))
+  if (! fgets(line,MAX_HEADER_LINE,st))
     {
       fprintf(stderr,"failed header read\n");
       return SAGREAD_ERROR;
@@ -56,7 +59,7 @@ extern int sagread_open(const char* path,sagread_t* S)
 
   char *magic;
 
-  if (!(magic = strtok(line," \t")))
+  if (!(magic = strtok(line,DELIM)))
     {
       fprintf(stderr,"failed read of magic\n");
       return SAGREAD_ERROR;
@@ -70,7 +73,7 @@ extern int sagread_open(const char* path,sagread_t* S)
 
   char *tok;
 
-  if ( !(tok = strtok(NULL," \t")))
+  if ( !(tok = strtok(NULL,DELIM)))
     {
       fprintf(stderr,"failed read of version\n");
       return SAGREAD_ERROR;
@@ -84,7 +87,7 @@ extern int sagread_open(const char* path,sagread_t* S)
       return SAGREAD_ERROR;
     }
 
-  if ( !(tok = strtok(NULL," \t")))
+  if ( !(tok = strtok(NULL,DELIM)))
     {
       fprintf(stderr,"failed read of grid dimension\n");
       return SAGREAD_ERROR;
@@ -98,7 +101,7 @@ extern int sagread_open(const char* path,sagread_t* S)
       return SAGREAD_ERROR;
     }
 
-  if ( !(tok = strtok(NULL," \t")))
+  if ( !(tok = strtok(NULL,DELIM)))
     {
       fprintf(stderr,"failed read of vector dimension\n");
       return SAGREAD_ERROR;
@@ -124,7 +127,7 @@ extern int sagread_open(const char* path,sagread_t* S)
 
   for (i=0 ; i<gdim ; i++)
     {
-      if ( !(tok = strtok(NULL," \t")))
+      if ( !(tok = strtok(NULL,DELIM)))
 	{
 	  fprintf(stderr,"failed read of dimension %i size\n",i);
 	  return SAGREAD_ERROR;
@@ -151,7 +154,7 @@ extern int sagread_open(const char* path,sagread_t* S)
 
   for (i=0 ; i<vdim ; i++)
     {
-      if ( !(tok = strtok(NULL," \t")))
+      if ( !(tok = strtok(NULL,DELIM)))
 	{
 	  fprintf(stderr,"failed read of vector componet %i minimum\n",i);
 	  return SAGREAD_ERROR;
@@ -159,7 +162,7 @@ extern int sagread_open(const char* path,sagread_t* S)
 
       double min = atof(tok);
 
-      if ( !(tok = strtok(NULL," \t")))
+      if ( !(tok = strtok(NULL,DELIM)))
 	{
 	  fprintf(stderr,"failed read of vector componet %i maximum\n",i);
 	  return SAGREAD_ERROR;
@@ -171,7 +174,7 @@ extern int sagread_open(const char* path,sagread_t* S)
       mm[i].max = max;
     }
 
-  if ( !(tok = strtok(NULL," \t")))
+  if ( !(tok = strtok(NULL,DELIM)))
     {
       fprintf(stderr,"failed read of tolerance\n");
       return SAGREAD_ERROR;
@@ -188,21 +191,127 @@ extern int sagread_open(const char* path,sagread_t* S)
   S->st = st;
   S->grid.dim = gdim;
   S->grid.n = n;
-  S->grid.interval = mm;
+  S->grid.bnd = mm;
   S->vector.dim = vdim;
   S->tol = tol;
 
   return SAGREAD_OK;
 }
 
-extern int sagread_line(sagread_t* S,size_t* n,double* x)
+/*
+  read a data line of the sag file
+  - n is the grid index of the read point,
+    an array of at least S.grid.dim indicies
+  - v is the vector at this point, with
+    S.vector.dim components
+
+  this function may return 
+  OK     - the values were read and set
+  NODATA - the read value was not on the grid
+  EOF    - end of file
+  ERROR  - duh
+
+  the arguments n and v may be modified even
+  if the return is not OK
+*/
+
+extern int sagread_line(sagread_t S,size_t* n,double* v)
 {
-  return SAGREAD_EOF;
+  char line[MAX_DATA_LINE];
+
+  if (ferror(S.st)) 
+    {
+      fprintf(stderr,"error on stream\n");
+      return SAGREAD_ERROR;
+    }
+
+  /* read line */
+
+  if (! fgets(line,MAX_HEADER_LINE,S.st))
+    {
+      if (feof(S.st)) return SAGREAD_EOF;
+
+      fprintf(stderr,"failed line read\n");
+      return SAGREAD_ERROR;
+    }
+
+  /* tokenise */
+
+  size_t i;
+  char *tok;
+  double x[S.grid.dim];
+
+  for (tok = strtok(line,DELIM), i=0 ; 
+       i<S.grid.dim ; 
+       tok = strtok(NULL,DELIM), i++)
+    {
+      if (!tok)
+	{
+	  fprintf(stderr,"failed x[%i] tokenise\n",i);
+	  return SAGREAD_ERROR;
+	}
+
+      x[i] = atof(tok);
+    }
+
+  for (i=0 ; 
+       i<S.vector.dim ; 
+       tok = strtok(NULL,DELIM), i++)
+    {
+      if (!tok)
+	{
+	  fprintf(stderr,"failed v[%i] tokenise\n",i);
+	  return SAGREAD_ERROR;
+	}
+
+      v[i] = atof(tok);
+    }
+
+  /* utility */
+
+  double dx[S.grid.dim];
+
+  for (i=0 ; i<S.grid.dim ; i++)
+    dx[i] = (S.grid.bnd[i].max - S.grid.bnd[i].min)/(S.grid.n[i]-1.0);
+
+  /* find index of grid node nearest x */
+
+  for (i=0 ; i<S.grid.dim ; i++)
+    {
+      n[i] = (x[i] - S.grid.bnd[i].min)/dx[i];
+
+      if ((n[i]<0) || (n[i]>S.grid.n[i]-1)) return SAGREAD_NODATA; 
+    }
+
+  /* find the x value at this grid node */
+
+  double y[S.grid.dim];
+
+  for (i=0 ; i<S.grid.dim ; i++)
+    {
+      y[i] = S.grid.bnd[i].min + n[i]*dx[i]; 
+
+      printf("%i %i %f | %f -> %i %f\n",(int)i,(int)S.grid.n[i],dx[i],x[i],n[i],y[i]);
+    }
+
+  /* distance squared between x and y */
+
+  double s2 = 0.0;
+
+  for (i=0 ; i<S.grid.dim ; i++)
+    {
+      double d = y[i] - x[i];
+      s2 += d*d;
+    }
+
+  if (s2 > S.tol*S.tol) return SAGREAD_NODATA;
+
+  return SAGREAD_OK;
 }
 
-extern void sagread_close(sagread_t* S)
+extern void sagread_close(sagread_t S)
 {
-  fclose(S->st);
-  free(S->grid.n);
-  free(S->grid.interval);
+  fclose(S.st);
+  free(S.grid.n);
+  free(S.grid.bnd);
 }
