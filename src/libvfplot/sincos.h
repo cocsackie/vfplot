c5debdbb5831fa6bae4426ebe0f764bcaaf9644c
J.J. Green,,, <j.j.green@gmx.co.uk>
2008-10-13 21:09:44 +0000

    *** empty log message ***

--- src/libvfplot/sincos.h
+++ src/libvfplot/sincos.h
@@ -2,18 +2,23 @@
   sincos.h
   conditonal sincos
   J.J.Green 2007
-  $Id$
+  $Id: sincos.h,v 1.1 2007/08/02 21:31:09 jjg Exp jjg $
 
   portability layer for sincos (which uses the x86
   sincos instruction), which is available in GNU 
   libc when _GNU_SOURCE is defined. We just declare
-  the function here, and define it only if 
-  HAVE_SINCOS is defined
+  and define it only if HAVE_SINCOS is defined
 */
 
 #ifndef SINCOS_H
 #define SINCOS_H
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifndef HAVE_SINCOS
 extern void sincos(double,double*,double*);
+#endif
 
 #endif
