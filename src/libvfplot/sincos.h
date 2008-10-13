471e69670e1fc2ae0b12871d2393b33fdc6b6f06
J.J. Green,,, <j.j.green@gmx.co.uk>
2008-10-13 21:21:39 +0000

    *** empty log message ***

--- src/libvfplot/sincos.h
+++ src/libvfplot/sincos.h
@@ -2,7 +2,7 @@
   sincos.h
   conditonal sincos
   J.J.Green 2007
-  $Id: sincos.h,v 1.2 2008/10/13 21:09:44 jjg Exp jjg $
+  $Id: sincos.h,v 1.3 2008/10/13 21:16:17 jjg Exp jjg $
 
   portability layer for sincos (which uses the x86
   sincos instruction), which is available in GNU 
@@ -13,14 +13,6 @@
 #ifndef SINCOS_H
 #define SINCOS_H
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#ifdef HAVE_SINCOS
-#define _GNU_SOURCE
-#else
 extern void sincos(double,double*,double*);
-#endif
 
 #endif
