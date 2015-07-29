dd071960a7d289cc702472d1de6712bbbb724eba
J.J. Green <j.j.green@gmx.co.uk>
2015-07-29 23:21:36 +0100

    unit test for domain_bbox

--- src/unit/test_domain.c
+++ src/unit/test_domain.c
@@ -11,6 +11,7 @@ CU_TestInfo tests_domain[] =
   {
     {"new", test_domain_new},
     {"read", test_domain_read},
+    {"bbox", test_domain_bbox},
     CU_TEST_INFO_NULL
   };
 
@@ -33,6 +34,28 @@ extern void test_domain_read(void)
   domain_t *dom = domain_read(path);
 
   CU_ASSERT_FATAL(dom != NULL);
+
+  domain_destroy(dom);
+}
+
+static domain_t* read_simple_dom(void)
+{
+  const char* path = fixture("simple.dom");
+  return domain_read(path);
+}
+
+extern void test_domain_bbox(void)
+{
+  domain_t *dom = read_simple_dom();
+  CU_ASSERT_FATAL(dom != NULL);
+
+  bbox_t bbox = domain_bbox(dom);
+  double eps = 1e-10;
+
+  CU_ASSERT_DOUBLE_EQUAL(bbox.x.min,  0, eps);
+  CU_ASSERT_DOUBLE_EQUAL(bbox.x.max, 60, eps);
+  CU_ASSERT_DOUBLE_EQUAL(bbox.y.min,  0, eps);
+  CU_ASSERT_DOUBLE_EQUAL(bbox.y.max, 60, eps);
 
   domain_destroy(dom);
 }
--- src/unit/test_domain.h
+++ src/unit/test_domain.h
@@ -9,3 +9,4 @@ extern CU_TestInfo tests_domain[];
 
 extern void test_domain_new(void);
 extern void test_domain_read(void);
+extern void test_domain_bbox(void);
