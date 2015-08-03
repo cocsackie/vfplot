1c9fd885edab3bd186a546f83abdc52bc9094d66
J.J. Green <j.j.green@gmx.co.uk>
2015-08-03 21:15:39 +0100

    spacing fixes

--- src/unit/test_vector.c
+++ src/unit/test_vector.c
@@ -8,22 +8,22 @@
 
 CU_TestInfo tests_vector[] =
   {
-    {"subtraction",test_vsub},
-    {"addition",test_vadd},
-    {"scalar multiplication",test_smul},
-    {"norm",test_vabs},
-    {"norm squared",test_vabs2},
-    {"vector-pair determinant",test_vdet},
-    {"scalar product",test_sprd},
-    {"angle",test_vang},
-    {"external angle",test_vxtang},
-    {"unit vector",test_vunit},
-    {"line projection",test_projline},
-    {"bend",test_bend},
+    {"subtraction", test_vsub},
+    {"addition", test_vadd},
+    {"scalar multiplication", test_smul},
+    {"norm", test_vabs},
+    {"norm squared", test_vabs2},
+    {"vector-pair determinant", test_vdet},
+    {"scalar product", test_sprd},
+    {"angle", test_vang},
+    {"external angle", test_vxtang},
+    {"unit vector", test_vunit},
+    {"line projection", test_projline},
+    {"bend", test_bend},
     CU_TEST_INFO_NULL
   };
 
-#define CU_ASSERT_VECT_EQUAL(u,v,eps) \
+#define CU_ASSERT_VECT_EQUAL(u, v, eps) \
   CU_ASSERT_DOUBLE_EQUAL(X(u), X(v), eps) ; \
   CU_ASSERT_DOUBLE_EQUAL(Y(u), Y(v), eps)
 
@@ -31,28 +31,28 @@ static double eps = 1e-10;
 
 extern void test_vsub(void)
 {
-  vector_t u[] = {VEC(0,1),VEC(1,0)}, v = vsub(u[0],u[1]), res = VEC(-1,1);
+  vector_t u[] = {VEC(0, 1), VEC(1, 0)}, v = vsub(u[0], u[1]), res = VEC(-1, 1);
 
   CU_ASSERT_VECT_EQUAL(v, res, eps);
 }
 
 extern void test_vadd(void)
 {
-  vector_t u[] = {VEC(0,1),VEC(1,0)}, v = vadd(u[0],u[1]), res = VEC(1,1);
+  vector_t u[] = {VEC(0, 1), VEC(1, 0)}, v = vadd(u[0], u[1]), res = VEC(1, 1);
 
   CU_ASSERT_VECT_EQUAL(v, res, eps);
 }
 
 extern void test_smul(void)
 {
-  vector_t u = VEC(0,1), v = smul(2.0,u), res = VEC(0,2);
+  vector_t u = VEC(0, 1), v = smul(2.0, u), res = VEC(0, 2);
 
   CU_ASSERT_VECT_EQUAL(v, res, eps);
 }
 
 extern void test_vabs(void)
 {
-  vector_t u = VEC(3,4);
+  vector_t u = VEC(3, 4);
   double d = vabs(u);
 
   CU_ASSERT_DOUBLE_EQUAL(d, 5.0, eps);
@@ -60,19 +60,19 @@ extern void test_vabs(void)
 
 extern void test_bend(void)
 {
-  vector_t a = VEC(1,0), b = VEC(0,0), c = VEC(0,1);
+  vector_t a = VEC(1, 0), b = VEC(0, 0), c = VEC(0, 1);
 
-  CU_ASSERT_EQUAL(bend_3pt(a,b,c),rightward);
-  CU_ASSERT_EQUAL(bend_3pt(c,b,a),leftward);
-  CU_ASSERT_EQUAL(bend_3pt(a,c,b),leftward);
-  CU_ASSERT_EQUAL(bend_3pt(b,c,a),rightward);
-  CU_ASSERT_EQUAL(bend_3pt(b,a,c),leftward);
-  CU_ASSERT_EQUAL(bend_3pt(c,a,b),rightward);
+  CU_ASSERT_EQUAL(bend_3pt(a, b, c), rightward);
+  CU_ASSERT_EQUAL(bend_3pt(c, b, a), leftward);
+  CU_ASSERT_EQUAL(bend_3pt(a, c, b), leftward);
+  CU_ASSERT_EQUAL(bend_3pt(b, c, a), rightward);
+  CU_ASSERT_EQUAL(bend_3pt(b, a, c), leftward);
+  CU_ASSERT_EQUAL(bend_3pt(c, a, b), rightward);
 }
 
 extern void test_vabs2(void)
 {
-  vector_t u = VEC(3,4);
+  vector_t u = VEC(3, 4);
   double d = vabs2(u);
 
   CU_ASSERT_DOUBLE_EQUAL(d, 25.0, eps);
@@ -80,16 +80,16 @@ extern void test_vabs2(void)
 
 extern void test_vdet(void)
 {
-  vector_t u = VEC(1,2), v = VEC(1,1);
-  double d = vdet(u,v);
+  vector_t u = VEC(1, 2), v = VEC(1, 1);
+  double d = vdet(u, v);
 
-  CU_ASSERT_DOUBLE_EQUAL(d,-1.0,eps);
+  CU_ASSERT_DOUBLE_EQUAL(d, -1.0, eps);
 }
 
 extern void test_sprd(void)
 {
-  vector_t u = VEC(3,4), v = VEC(1,2);
-  double d = sprd(u,v);
+  vector_t u = VEC(3, 4), v = VEC(1, 2);
+  double d = sprd(u, v);
 
   CU_ASSERT_DOUBLE_EQUAL(d, 11.0, eps);
 }
@@ -98,23 +98,23 @@ extern void test_sprd(void)
 
 extern void test_vang(void)
 {
-  vector_t u[5] = {VEC(4,0),
-		   VEC(4,4),
-		   VEC(4,-4),
-		   VEC(-4,4),
-		   VEC(-4,-4)};
+  vector_t u[5] = {VEC(4, 0),
+		   VEC(4, 4),
+		   VEC(4, -4),
+		   VEC(-4, 4),
+		   VEC(-4, -4)};
 
-  CU_ASSERT_DOUBLE_EQUAL(vang(u[0]),0,eps);
-  CU_ASSERT_DOUBLE_EQUAL(vang(u[1]),PI4,eps);
-  CU_ASSERT_DOUBLE_EQUAL(vang(u[2]),-PI4,eps);
-  CU_ASSERT_DOUBLE_EQUAL(vang(u[3]),3*PI4,eps);
-  CU_ASSERT_DOUBLE_EQUAL(vang(u[4]),-3*PI4,eps);
+  CU_ASSERT_DOUBLE_EQUAL(vang(u[0]), 0, eps);
+  CU_ASSERT_DOUBLE_EQUAL(vang(u[1]), PI4, eps);
+  CU_ASSERT_DOUBLE_EQUAL(vang(u[2]), -PI4, eps);
+  CU_ASSERT_DOUBLE_EQUAL(vang(u[3]), 3*PI4, eps);
+  CU_ASSERT_DOUBLE_EQUAL(vang(u[4]), -3*PI4, eps);
 }
 
 extern void test_vxtang(void)
 {
-  vector_t u = VEC(4,0), v = VEC(0,4);
-  double d = vxtang(u,v);
+  vector_t u = VEC(4, 0), v = VEC(0, 4);
+  double d = vxtang(u, v);
 
   CU_ASSERT_DOUBLE_EQUAL(d, M_PI/2.0, eps);
 }
@@ -122,9 +122,9 @@ extern void test_vxtang(void)
 extern void test_vunit(void)
 {
   vector_t
-    u = VEC(1,1),
+    u = VEC(1, 1),
     v = vunit(u),
-    res = VEC(1.0/sqrt(2.0),1.0/sqrt(2.0));
+    res = VEC(1.0/sqrt(2.0), 1.0/sqrt(2.0));
 
   CU_ASSERT_VECT_EQUAL(v, res, eps);
 }
@@ -132,12 +132,12 @@ extern void test_vunit(void)
 extern void test_projline(void)
 {
   vector_t
-    p = VEC(0,0),
-    v[4] = {VEC(1,0), VEC(0,1), VEC(-1,0), VEC(0,-1)},
-    x = VEC(3,1);
+    p = VEC(0, 0),
+    v[4] = {VEC(1, 0), VEC(0, 1), VEC(-1, 0), VEC(0, -1)},
+    x = VEC(3, 1);
 
-  CU_ASSERT_DOUBLE_EQUAL(projline(p,v[0],x),3,eps);
-  CU_ASSERT_DOUBLE_EQUAL(projline(p,v[1],x),1,eps);
-  CU_ASSERT_DOUBLE_EQUAL(projline(p,v[2],x),-3,eps);
-  CU_ASSERT_DOUBLE_EQUAL(projline(p,v[3],x),-1,eps);
+  CU_ASSERT_DOUBLE_EQUAL(projline(p, v[0], x), 3, eps);
+  CU_ASSERT_DOUBLE_EQUAL(projline(p, v[1], x), 1, eps);
+  CU_ASSERT_DOUBLE_EQUAL(projline(p, v[2], x), -3, eps);
+  CU_ASSERT_DOUBLE_EQUAL(projline(p, v[3], x), -1, eps);
 }
