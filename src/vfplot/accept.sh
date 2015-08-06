282f649677d43777f6e64654c82b14cfe92d5d5f
J.J. Green <j.j.green@gmx.co.uk>
2015-08-06 20:03:11 +0100

    acceptance test stubs for hedgehog

--- src/vfplot/accept.sh
+++ src/vfplot/accept.sh
@@ -2,12 +2,26 @@
 
 source "$2/accept-setup.sh"
 
-# can we create the test-plots
+# common geometric options
 
-for plot in cylinder electro2 electro3
+geometry="-s0.75 -m4/4/0 -w4i"
+
+# create a hedgehog plot of all standard plots
+
+for plot in circular cylinder electro2 electro3
 do
     eps="$plot.eps"
-    cmd="./vfplot -s0.75 -m4/4/0 -w4i -i30/5 -t $plot -o $eps"
+    cmd="./vfplot -p hedgehog $geometry -t $plot -o $eps"
+    assert_raises "$cmd" 0
+    rm -f $eps
+done
+
+# create an adaptive plot
+
+for plot in cylinder
+do
+    eps="$plot.eps"
+    cmd="./vfplot -p adaptive -i30/5 $geometry -t $plot -o $eps"
     assert_raises "$cmd" 0
     rm -f $eps
 done
