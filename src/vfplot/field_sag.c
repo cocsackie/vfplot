f326252211f8f3d63e2e4b47564c3ebca4d633b0
J.J. Green <j.j.green@gmx.co.uk>
2016-05-24 22:27:40 +0100

    tidying and an annotation of field_sag.c

--- src/vfplot/field_sag.c
+++ src/vfplot/field_sag.c
@@ -1,4 +1,4 @@
-/* 
+/*
    sag (simple ascii grid) format (see libvfplot/sagread.c)
 
    Copyright (c) J.J. Green 2013
@@ -20,59 +20,60 @@ extern field_t* field_read_sag(const char* file)
 {
   sagread_t S;
 
-  if (sagread_open(file,&S) !=  SAGREAD_OK)
+  if (sagread_open(file, &S) !=  SAGREAD_OK)
     {
-      fprintf(stderr,"failed open of %s\n",file);
+      fprintf(stderr, "failed open of %s\n", file);
       return NULL;
     }
 
   if (S.grid.dim != 2)
     {
-      fprintf(stderr,"file %s is a %i dimensional grid (2 required)\n",
-	      file,(int)S.grid.dim);
+      fprintf(stderr, "file %s is a %i dimensional grid (2 required)\n",
+	      file, (int)S.grid.dim);
       return NULL;
     }
 
   if (S.vector.dim != 2)
     {
-      fprintf(stderr,"file %s uses %i dimensional vectors (2 required)\n",
-	      file,(int)S.vector.dim);
+      fprintf(stderr, "file %s uses %i dimensional vectors (2 required)\n",
+	      file, (int)S.vector.dim);
       return NULL;
     }
 
   bilinear_t *B[2];
-  size_t i;
-  bbox_t bb = 
+  bbox_t bb =
     {{S.grid.bnd[0].min, S.grid.bnd[0].max},
      {S.grid.bnd[1].min, S.grid.bnd[1].max}};
 
-  for (i=0 ; i<2 ; i++)
+  for (size_t i = 0 ; i < 2 ; i++)
     {
       if ((B[i] = bilinear_new()) == NULL) return NULL;
-      bilinear_dimension(S.grid.n[0],S.grid.n[1],bb,B[i]);
+      bilinear_dimension(S.grid.n[0], S.grid.n[1], bb, B[i]);
     }
 
+  size_t read = 0, set = 0;
   int err;
-  size_t n[2],read=0,set=0;
-  double x[2];
 
-  do 
+  do
     {
-      switch (err = sagread_line(S,n,x))
+      size_t n[2];
+      double x[2];
+
+      switch (err = sagread_line(S, n, x))
 	{
 	case SAGREAD_OK:
 	  set++;
-	  for (i=0 ; i<2 ; i++) 
-	    bilinear_setz(n[0],n[1],x[i],B[i]);
-
+	  for (size_t i = 0 ; i < 2 ; i++)
+	    bilinear_setz(n[0], n[1], x[i], B[i]);
+	  /* fallthrough */
 	case SAGREAD_NODATA:
 	  read++;
-
+	  /* fallthrough */
 	case SAGREAD_EOF:
 	  break;
 
-	default: 
-	  fprintf(stderr,"error at line %i\n",(int)(read+1));
+	default:
+	  fprintf(stderr, "error at line %i\n", (int)(read+1));
 	  return NULL;
 	}
     }
@@ -80,19 +81,19 @@ extern field_t* field_read_sag(const char* file)
 
   sagread_close(S);
 
-  if (read == 0)
+  if (! read)
     {
-      fprintf(stderr,"sag file has no data lines\n");
+      fprintf(stderr, "sag file has no data lines\n");
       return NULL;
     }
 
-  if (set == 0)
+  if (! set)
     {
-      fprintf(stderr,"read %i nodata lines - bad sag header?\n",(int)read);
+      fprintf(stderr, "read %i nodata lines - bad sag header?\n", (int)read);
       return NULL;
     }
 
-  field_t* F = malloc(sizeof(field_t));
+  field_t *F = malloc(sizeof(field_t));
 
   if (!F) return NULL;
 
